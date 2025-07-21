019beaf391c19a8f83c35ec220d6160c85ec9c80
Bryson77 <109231608+Bryson77@users.noreply.github.com>
2025-07-21 21:28:02 +0200

    Empty

---
+++ Appcode.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Appcode {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Code for the app goes here
+        scanner.close();
+    }
+}
